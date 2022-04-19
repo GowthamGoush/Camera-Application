17b39502fd521692698b4e240c5bd0eb1e22fb97
GowthamGoush <westcity.21.gowtham@gmail.com>
2022-04-19 09:56:16 +0530

    Update utility func

--- lib/utilities/utility_functions.dart
+++ lib/utilities/utility_functions.dart
@@ -18,3 +18,19 @@ List<Point> generateMultipleRandomPoints(int maxWidth, int maxHeight) {
 
   return points;
 }
+
+bool isInsidePolygon(Point point, List vs) {
+  var x = point.x, y = point.y;
+
+  var inside = false;
+  for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+    var xi = vs[i][0], yi = vs[i][1];
+    var xj = vs[j][0], yj = vs[j][1];
+
+    var intersect = ((yi > y) != (yj > y))
+        && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+    if (intersect) inside = !inside;
+  }
+
+  return inside;
+}
