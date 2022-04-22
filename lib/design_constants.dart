8a4f19b41f565bec0cf9f22565d33d8632738f1d
GowthamGoush <westcity.21.gowtham@gmail.com>
2022-04-22 10:53:13 +0530

    Update constants

--- lib/design_constants.dart
+++ lib/design_constants.dart
@@ -1,5 +1,13 @@
-
+import 'package:flutter/material.dart';
 
 /// Canvas constants
 const recWidth = 200.0, recHeight = 150.0;
 const circleRadius = 5.0;
+const maxPoints = 5;
+
+/// Text constants
+const simpleTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+  fontSize: 16.0,
+);
